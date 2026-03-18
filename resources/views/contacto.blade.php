13646c50c303363afd936580d7f66e04b45478ee
Santiago <kevinjojoa24@gmail.com>
2026-03-18 10:48:43 -0500

    guardar hoy

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -41,6 +41,44 @@ border:none;
 
 <body>
 
+<!-- BARRA DE NAVEGACIÓN -->
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container-fluid">
+
+<a class="navbar-brand" href="/">Premium Motors</a>
+
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarNav">
+
+<ul class="navbar-nav ms-auto">
+
+<li class="nav-item">
+<a class="nav-link" href="/">Inicio</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="/menu">Autos</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="/nosotros">Nosotros</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link active" href="/contacto">Contacto</a>
+</li>
+
+</ul>
+
+</div>
+</div>
+</nav>
+
+
 <div class="container">
 
 <h1 class="text-center mt-5">Contacto</h1>
@@ -102,6 +140,8 @@ border:none;
 </div>
 
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
