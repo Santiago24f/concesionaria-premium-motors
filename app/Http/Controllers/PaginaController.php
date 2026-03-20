82a3b901a6548cfb5f6cb2a2c2391a86c8b3a86c
Santiago <kevinjojoa24@gmail.com>
2026-03-20 09:21:22 -0500

    20 de marzo

---
+++ app/Http/Controllers/PaginaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaginaController extends Controller
+{
+    public function inicio(){
+        return view('inicio');
+    }
+    public function nosotros(){
+        return view('nosotros');
+    }
+    public function contacto(){
+        return view('contacto');
+    }
+}
